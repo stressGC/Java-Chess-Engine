cdcb1b82b2322797e044900aee5b1ab39d9fc59e
stressGC <g.cosson@live.fr>
2018-12-12 01:07:06 -0500

    removing useless files

--- src/georges/cosson/Engine.java
+++ src/georges/cosson/Engine.java
@@ -8,7 +8,8 @@ public class Engine {
 	// Bitboards
     static long WP=0L,WN=0L,WB=0L,WR=0L,WQ=0L,WK=0L,BP=0L,BN=0L,BB=0L,BR=0L,BQ=0L,BK=0L,EP=0L;
     
-    static boolean CWK=true,CWQ=true,CBK=true,CBQ=true,WhiteToMove=true;//true=castle is possible
+    //true=castle is possible
+    static boolean CWK=true,CWQ=true,CBK=true,CBQ=true,WhiteToMove=true;
     
     static long UniversalWP=0L,UniversalWN=0L,UniversalWB=0L,UniversalWR=0L,
             UniversalWQ=0L,UniversalWK=0L,UniversalBP=0L,UniversalBN=0L,
--- src/georges/cosson/Rating.java
+++ src/georges/cosson/Rating.java
@@ -1,14 +1,8 @@
 package georges.cosson;
 
-import java.util.Random;
-
 public class Rating {
-	//static int score = 0;
-	static int max = 200;
-	static int min = 0;
-	static int[] scores = {55, 50, 40, 37, 39, 45, 11, -2, 2, 7, 59};
-	static Random rand = new Random(); 
 	
+	// pieces values
 	static int PAWN_VALUE = 100;
 	static int KNIGHT_VALUE = 325;
 	static int BISHOP_VALUE = 330;
@@ -16,24 +10,13 @@ public class Rating {
 	static int QUEEN_VALUE = 900;
 	static int KING_VALUE = 20000;
 	
+	/*
+	 * given a state of board, returns an estimator of the score
+	 * 
+	 * simply counts the pieces remaining on both sides, and computes the difference between them
+	 */
+	
     public static int evaluate(boolean whiteTurn, long WP,long WN,long WB,long WR,long WQ,long WK,long BP,long BN,long BB,long BR,long BQ,long BK,long EP,boolean CWK,boolean CWQ,boolean CBK,boolean CBQ) {
-    	//int score = rand.nextInt(max - min + 1) + min;
-    	//score++;
-    	/*
-    	if ((WK & Moves.unsafeForWhite(WP, WN, WB, WR, WQ, WK, BP, BN, BB, BR, BQ, BK)) == 0 && whiteTurn) {
-    		System.out.println("==");
-    		return -Engine.MATE_SCORE;
-    	}
-    	if ((WK & Moves.unsafeForWhite(WP, WN, WB, WR, WQ, WK, BP, BN, BB, BR, BQ, BK)) == 0 && !whiteTurn) {
-    		return Engine.MATE_SCORE;
-    	}
-    	if ((BK & Moves.unsafeForBlack(WP, WN, WB, WR, WQ, WK, BP, BN, BB, BR, BQ, BK)) == 0 && !whiteTurn) {
-    		return -Engine.MATE_SCORE;
-    	}
-    	if ((BK & Moves.unsafeForBlack(WP, WN, WB, WR, WQ, WK, BP, BN, BB, BR, BQ, BK)) == 0 && whiteTurn) {
-    		return Engine.MATE_SCORE;
-    	}*/
-    	
     	
     	int score = 0;
     	score += whiteTurn ? getEvaluation(WP, BP, PAWN_VALUE) : -getEvaluation(WP, BP, PAWN_VALUE);
@@ -41,24 +24,16 @@ public class Rating {
     	score += whiteTurn ? getEvaluation(WB, BB, BISHOP_VALUE) : -getEvaluation(WB, BB, BISHOP_VALUE);
     	score += whiteTurn ? getEvaluation(WR, BR, ROOK_VALUE) : -getEvaluation(WR, BR, ROOK_VALUE);
     	score += whiteTurn ? getEvaluation(WQ, BQ, QUEEN_VALUE) : -getEvaluation(WQ, BQ, QUEEN_VALUE);
-    	//System.out.println("whiteTurn={"+whiteTurn+"}" + " score = " + score + ", whiteP={" + numberOfOnes(WP) + "} " + ", blackP={" + numberOfOnes(BP) + "} ");
-    	//if (((WKt&Moves.unsafeForWhite(WPt,WNt,WBt,WRt,WQt,WKt,BPt,BNt,BBt,BRt,BQt,BKt))==0 && WhiteToMove) ||
-        //((BKt&Moves.unsafeForBlack(WPt,WNt,WBt,WRt,WQt,WKt,BPt,BNt,BBt,BRt,BQt,BKt))==0 && !WhiteToMove)) {
-    	
-    	//int indexRnd = rand.nextInt(scores.length);
-        //return scores[indexRnd];
+    	score += whiteTurn ? getEvaluation(WK, BK, KING_VALUE) : -getEvaluation(WK, BK, KING_VALUE);
+
     	return score;
     }
     
-    static int numberOfOnes(long aa) {
-    	return Long.bitCount(aa);
-    }
-    
+    // returns the difference of score based on a type of piece
     static int getEvaluation(Long W, Long B, int valuePerCapita) {
-    	int scoreW = numberOfOnes(W);
-    	int scoreB = numberOfOnes(B);
-    	int difference = scoreW - scoreB;
-    	return difference * valuePerCapita;
+    	int scoreW = Long.bitCount(W);
+    	int scoreB = Long.bitCount(B);
+    	return (scoreW - scoreB) * valuePerCapita;
     }
 
 }
